f2d1ae2c71f3be88d81ccb810c0e5eefc5c487d0
William <willtheorangeguy@users.noreply.github.com>
2021-08-28 23:20:15 -0700

    Fix scripts

--- 2_download.sh
+++ 2_download.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# List of Playlists:
+#"https://www.deezer.com/en/playlist/9410154282" 
+#"https://www.deezer.com/en/playlist/9410156002" 
+#"https://www.deezer.com/en/playlist/9410145502"
+#"https://www.deezer.com/en/playlist/9410148262"            
+#"https://www.deezer.com/en/playlist/9410150302"
+#"https://www.deezer.com/en/playlist/9410146342"
+#"https://www.deezer.com/en/playlist/9410152902" 
+#"https://www.deezer.com/en/playlist/9416521802"
+
 # Iterate through Deezer playlists
-declare -a playlists=("https://www.deezer.com/en/playlist/9410154282" 
-                      "https://www.deezer.com/en/playlist/9410156002" 
-                      "https://www.deezer.com/en/playlist/9410145502"
-                      "https://www.deezer.com/en/playlist/9410148262"            
-                      "https://www.deezer.com/en/playlist/9410150302"
-                      "https://www.deezer.com/en/playlist/9410146342"
-                      "https://www.deezer.com/en/playlist/9410152902"                                       
+declare -a playlists=( "https://www.deezer.com/en/playlist/9416521802"                                   
                       )
 for i in "${playlists[@]}"
 do
