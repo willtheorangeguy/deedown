8322da89c5167569c94efc3c99ad7e8c4c0fa8fe
William <willtheorangeguy@users.noreply.github.com>
2022-04-16 23:33:53 -0700

    Update progran code

--- 1_playlists.cmd
+++ 1_playlists.cmd
@@ -1,3 +1,18 @@
+rem deedown - Automatic Spotify to Deezer playlist updater, and deemix track downloader. 
+rem Copyright (C) 2020-2022 @willtheorangeguy
+
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, version 3 of the License.
+
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 @echo off
 
 rem Remove playlist files
